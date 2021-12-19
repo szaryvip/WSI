3273dc5a8277cb0df506fdbbcbb84ccc86b2ac3e
Szymon Skarzyński <01158894@pw.edu.pl>
2021-12-20 00:55:31 +0100

    modification of Layer class

--- NeuralNetwork/src/layer.py
+++ NeuralNetwork/src/layer.py
@@ -1,10 +1,13 @@
 class Layer:
     def __init__(self, neurons: list):
         self._neurons = neurons
-        self._next_layer = None
+        # self._next_layer = None
 
-    def get_next_layer(self):
-        return self._next_layer
+    def get_neurons(self):
+        return self._neurons
 
-    def set_next_layer(self, next_layer):
-        self._next_layer = next_layer
+    # def get_next_layer(self):
+    #     return self._next_layer
+
+    # def set_next_layer(self, next_layer):
+    #     self._next_layer = next_layer
