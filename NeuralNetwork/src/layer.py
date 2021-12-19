2a749fddc85fc37bdba0f62a1def8c90de2a2138
Szymon Skarzyński <01158894@pw.edu.pl>
2021-12-19 22:33:37 +0100

    improvements of neuron and layer classes

--- NeuralNetwork/src/layer.py
+++ NeuralNetwork/src/layer.py
@@ -1,10 +1,10 @@
 class Layer:
-    _next_layer = None
-    _neurons = []
-    
-    def __init__(self):
-        pass
-    
+    def __init__(self, neurons):
+        self._neurons = neurons
+        self._next_layer = None
+
     def get_next_layer(self):
         return self._next_layer
-    
+
+    def set_next_layer(self, next_layer):
+        self._next_layer = next_layer
--- NeuralNetwork/src/neuron.py
+++ NeuralNetwork/src/neuron.py
@@ -1,28 +1,23 @@
 class Neuron:
-    _weights = None
-    _delta = None
-    _output = None
-    
-    def __init__(self):
-        self._weights
-        self._output
-        self._delta
-        
+    def __init__(self, weights):
+        self._weights = weights
+        self._output = 0
+        self._delta = 0
+
     def get_weight(self):
         return self._weights
-        
+
     def set_weight(self, weight, index):
         self._weights[index] = weight
-    
+
     def get_output(self):
         return self._output
-        
+
     def set_output(self, output):
         self._output = output
-        
+
     def get_delta(self):
         return self._delta
-        
+
     def set_delta(self, delta):
         self._delta = delta
-    
