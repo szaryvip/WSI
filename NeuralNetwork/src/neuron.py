1f0cf30a0fdacae7b6f9500bbed9410c94c8f436
Łukasz Szarejko <lukaszszarejko@wp.pl>
2021-12-19 20:48:01 +0100

    first implementation of neuron

--- NeuralNetwork/src/neuron.py
+++ NeuralNetwork/src/neuron.py
@@ -1,2 +1,28 @@
 class Neuron:
-    pass
+    _weight = None
+    _delta = None
+    _output = None
+    
+    def __init__(self):
+        self._weight
+        self._output
+        self._delta
+        
+    def get_weight(self):
+        return self._weight
+        
+    def set_weight(self, weight):
+        self._weight = weight
+    
+    def get_output(self):
+        return self._output
+        
+    def set_output(self, output):
+        self._output = output
+        
+    def get_delta(self):
+        return self._delta
+        
+    def set_delta(self, delta):
+        self._delta = delta
+    
