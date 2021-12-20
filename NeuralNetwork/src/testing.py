cb1dfbe6da85bcd2f9989fff5e5933d9b82b98d0
Szymon Skarzyński <01158894@pw.edu.pl>
2021-12-20 17:41:33 +0100

    added TODO list in testig.py

--- NeuralNetwork/src/testing.py
+++ NeuralNetwork/src/testing.py
@@ -1,2 +1,12 @@
 import matplotlib as plt
 
+# TODO:
+# data analysis
+# neurons number
+# lauers number
+# learning rate
+# epochs
+# example network
+# example classifications
+# error matrix
+# quality metrics
