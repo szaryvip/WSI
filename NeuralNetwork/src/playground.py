e2fc204ebbe0048f91b76c9fe288573dbd829185
Łukasz Szarejko <lukaszszarejko@wp.pl>
2021-12-21 12:29:02 +0100

    tensorflow model add

--- NeuralNetwork/src/playground.py
+++ NeuralNetwork/src/playground.py
@@ -1,32 +1,78 @@
 from data_loader import load_data
 from neural_network import NeuralNetwork
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import precision_score
+
+from testing import test
+
+class_names = [i for i in range(10)]
+pred = []
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+  pred.append(predicted_label)
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
 
 if __name__ == "__main__":
-    training_data = load_data(
-        'data/train-images.idx3-ubyte',
-        'data/train-labels.idx1-ubyte'
-    )
-    test_data = load_data(
-        'data/t10k-images.idx3-ubyte',
-        'data/t10k-labels.idx1-ubyte'
-    )
-    # print(train[0])
+    testing_data = load_data('data/t10k-images.idx3-ubyte', 'data/t10k-labels.idx1-ubyte')
+    testing_labels = [data[1] for data in testing_data]
+    mnist = tf.keras.datasets.mnist
 
-    print('Data Loaded')
-    # quit()
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28,28)),
+        tf.keras.layers.Dense(10, activation='sigmoid'),
+        tf.keras.layers.Dense(10)  
+    ])
 
-    Chad = NeuralNetwork(
-        hidden_layers_number=2,
-        neurons_in_layer_number=10,
-        epochs_number=5
-    )
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
 
-    print(
-        Chad.back_propagation(
-            training_data[:1000],
-            test_data[:20],
-            1
-        )
-    )
+    model.fit(train_images, train_labels, epochs=10)
 
-    print([i[1] for i in test_data[:20]])
+    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+    print('\nTest accuracy:', test_acc)
+
+    probability_model = tf.keras.Sequential([model, 
+                                            tf.keras.layers.Softmax()])
+
+    predictions = probability_model.predict(test_images)
+    # print(precision_score(testing_labels, pred, labels=class_names, average='micro'))
+      
+    i = 0
+    plt.figure(figsize=(6,3))
+    plt.subplot(1,2,1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(1,2,2)
+    plot_value_array(i, predictions[i],  test_labels)
+    plt.show()
