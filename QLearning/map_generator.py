936e4d6e4e13a29194985509e04b5943fc52608c
Łukasz Szarejko <lukaszszarejko@wp.pl>
2021-12-27 22:36:17 +0100

    map_generator works correctly

--- QLearning/map_generator.py
+++ QLearning/map_generator.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
-from typing import Tuple
+from time import sleep
+from typing import Tuple, List
 
 def generate_map(
     start_point: Tuple[int, int],
@@ -23,11 +24,50 @@ def generate_map(
     return board
 
 
+def add_next_moves(point: Tuple[int, int],
+                   visited: np.ndarray) -> List:
+    moves = []
+    if point[0]-1 >= 0:
+        if visited[point[1],point[0]-1] == 0:
+            moves.append((point[0]-1, point[1]))
+    if point[0]+1 < len(visited[0]):        
+        if visited[point[1],point[0]+1] == 0:
+            moves.append((point[0]+1, point[1]))
+    if point[1]-1 >= 0:        
+        if visited[point[1]-1,point[0]] == 0:
+            moves.append((point[0], point[1]-1))
+    if point[1]+1 < len(visited):        
+        if visited[point[1]+1,point[0]] == 0:
+            moves.append((point[0], point[1]+1))
+    return moves
+
+
 def is_correct(
     mymap: np.ndarray,
     start_point: Tuple[int, int]
 ) -> bool:
-    return True
+    tiles_visited = np.zeros([len(mymap), len(mymap[0])])
+    tiles_visited[start_point[1], start_point[0]] = 1
+    to_check = add_next_moves(start_point, tiles_visited)
+    while len(to_check) != 0:
+        point = to_check[0]
+        if tiles_visited[point[1], point[0]] == 1:
+            to_check.remove(point)
+            continue
+        else:
+            tiles_visited[point[1], point[0]] = 1
+        if mymap[point[1], point[0]] == 2:
+            return True
+        elif mymap[point[1], point[0]] == -1:
+            to_check.remove(point)
+            continue
+        else:
+            check = to_check
+            check.remove(point)
+            to_check = add_next_moves(point, tiles_visited)
+            for move in check:
+                to_check.append(move)
+    return False
 
 
 def prepare_correct_map(
